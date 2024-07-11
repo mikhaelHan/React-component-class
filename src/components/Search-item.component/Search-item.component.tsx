e643fe3e71f7f4aaaae1461c20267ec7e95902af
mikhaelHan <hancharuk270185@mail.ru>
2024-07-11 10:19:41 +0300

    refactor: refactor search and search-item components

--- src/components/Search-item.component/Search-item.component.tsx
+++ src/components/Search-item.component/Search-item.component.tsx
@@ -3,33 +3,30 @@ import React from 'react';
 import './Search-item.component.scss';
 import { ISearchItem } from '../../models/Search.model';
 
-class SearchItemComponent extends React.Component<ISearchItem> {
-  render(): React.ReactNode {
-    const { name, gender, height, mass, eye_color } = this.props;
+const SearchItemComponent: React.FC<ISearchItem> = (props) => {
+  const { name, gender, height, mass, eye_color } = props;
 
-    return (
-      <div
-        className="search-item-container"
-        style={{ border: `0.2rem solid ${eye_color}` }}
-      >
-        <h3 className="search-item-container__title">{name}</h3>
-        <div className="search-item-container__info">
-          <p className="search-item-container__item">
-            Gender:{' '}
-            <span className="search-item-container__item-bold">{gender}</span>
-          </p>
-          <p className="search-item-container__item">
-            Height:{' '}
-            <span className="search-item-container__item-bold">{height}</span>
-          </p>
-          <p className="search-item-container__item">
-            Mass:{' '}
-            <span className="search-item-container__item-bold">{mass}</span>
-          </p>
-        </div>
+  return (
+    <div
+      className="search-item-container"
+      style={{ border: `0.2rem solid ${eye_color}` }}
+    >
+      <h3 className="search-item-container__title">{name}</h3>
+      <div className="search-item-container__info">
+        <p className="search-item-container__item">
+          Gender:{' '}
+          <span className="search-item-container__item-bold">{gender}</span>
+        </p>
+        <p className="search-item-container__item">
+          Height:{' '}
+          <span className="search-item-container__item-bold">{height}</span>
+        </p>
+        <p className="search-item-container__item">
+          Mass: <span className="search-item-container__item-bold">{mass}</span>
+        </p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SearchItemComponent;
--- src/components/Search.component/Search.component.tsx
+++ src/components/Search.component/Search.component.tsx
@@ -1,67 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Search.component.scss';
 
-class SearchComponent extends React.Component<
-  { onSearchChange: (searchValue: string) => void },
-  { search: string; toggle: boolean }
-> {
-  constructor(props: { onSearchChange: (searchValue: string) => void }) {
-    super(props);
-    this.state = { search: '', toggle: false };
+const SearchComponent: React.FC<{
+  onSearchChange: (searchValue: string) => void;
+}> = (props) => {
+  const [searchState, setSearchState] = useState<string>('');
+  const [toggleState, setToggleState] = useState<boolean>(false);
 
-    this.inputOnChange = this.inputOnChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.throwError = this.throwError.bind(this);
-  }
-
-  public handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    const { search } = this.state;
-    const { onSearchChange } = this.props;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const { onSearchChange } = props;
     e.preventDefault();
-    onSearchChange(search);
-    this.setState({ search: '' });
+    onSearchChange(searchState);
+    setSearchState('');
   };
 
-  public inputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const inputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const val = e.currentTarget.value;
-    this.setState({ search: val });
+    setSearchState(val);
   };
 
-  public throwError = () => {
-    this.setState({ toggle: true });
+  const throwError = () => {
+    setToggleState(true);
   };
 
-  render(): React.ReactNode {
-    const { search, toggle } = this.state;
-    if (toggle) throw new Error('Oooops, something went wrong...');
-    return (
-      <div className="search-container">
-        <form onSubmit={this.handleSubmit} className="search-container__form">
-          <input
-            onChange={this.inputOnChange}
-            value={search}
-            placeholder="Search..."
-            className="search-container__input"
-            type="search"
-            size={30}
-          />
-          <input
-            value="Search"
-            type="submit"
-            className="search-container__button"
-          />
-        </form>
-        <button
-          onClick={this.throwError}
-          type="button"
+  if (toggleState) throw new Error('Oooops, something went wrong...');
+  return (
+    <div className="search-container">
+      <form onSubmit={handleSubmit} className="search-container__form">
+        <input
+          onChange={inputOnChange}
+          value={searchState}
+          placeholder="Search..."
+          className="search-container__input"
+          type="search"
+          size={30}
+        />
+        <input
+          value="Search"
+          type="submit"
           className="search-container__button"
-        >
-          Error
-        </button>
-      </div>
-    );
-  }
-}
+        />
+      </form>
+      <button
+        onClick={throwError}
+        type="button"
+        className="search-container__button"
+      >
+        Error
+      </button>
+    </div>
+  );
+};
 
 export default SearchComponent;
