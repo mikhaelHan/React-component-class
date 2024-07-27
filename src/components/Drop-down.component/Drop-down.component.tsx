523d3d98d6eeaaf2ec5690fd09564a149a730215
mikhaelHan <hancharuk270185@mail.ru>
2024-07-28 02:08:08 +0300

    feat: add change theme for drop-down component

--- src/components/Drop-down.component/Drop-down.component.tsx
+++ src/components/Drop-down.component/Drop-down.component.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useAppSelector, useAppDispatch } from '../../redux/hook';
 import { removeAllCards } from '../../redux/counterSlice';
 
 import './Drop-down.component.scss';
+import { ThemeContext } from '../../services/Theme.provider';
 
 const DropDownComponent: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -14,6 +15,8 @@ const DropDownComponent: React.FC = () => {
     (state) => state.checkedCards.IdCards.length,
   );
 
+  const { isDarkTheme } = useContext(ThemeContext);
+
   return (
     <div
       className={`${visible ? 'drop-down-container active' : 'drop-down-container'}`}
@@ -21,20 +24,24 @@ const DropDownComponent: React.FC = () => {
       <button
         onClick={() => dispatch(removeAllCards())}
         type="button"
-        className="drop-down-container__button drop-down-container__cancel"
+        className={`drop-down-container__button drop-down-container__button ${isDarkTheme ? 'dark' : 'light'}`}
       >
         Cancel
       </button>
       <button
         type="button"
-        className="drop-down-container__button drop-down-container__load"
+        className={`drop-down-container__button drop-down-container__button ${isDarkTheme ? 'dark' : 'light'}`}
       >
         Load
       </button>
       <p className="drop-down-container__condition-text">
         Condition
         {'  '}
-        <span className="drop-down-container__condition">{condition}</span>
+        <span
+          className={`drop-down-container__condition drop-down-container__condition ${isDarkTheme ? 'dark' : 'light'}`}
+        >
+          {condition}
+        </span>
       </p>
     </div>
   );
