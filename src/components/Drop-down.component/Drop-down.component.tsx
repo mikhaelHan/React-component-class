f1f57340937244315a09713f6e53fc572ad4916e
mikhaelHan <hancharuk270185@mail.ru>
2024-07-28 03:13:02 +0300

    feat: add download data to csv file

--- src/components/Drop-down.component/Drop-down.component.tsx
+++ src/components/Drop-down.component/Drop-down.component.tsx
@@ -1,21 +1,43 @@
 import React, { useContext } from 'react';
+import Papa from 'papaparse';
+
 import { useAppSelector, useAppDispatch } from '../../redux/hook';
 import { removeAllCards } from '../../redux/counterSlice';
 
 import './Drop-down.component.scss';
 import { ThemeContext } from '../../services/Theme.provider';
+import { CheckedCard } from '../../models/redux.model';
 
 const DropDownComponent: React.FC = () => {
   const dispatch = useAppDispatch();
 
-  const visible = useAppSelector(
+  const visible: boolean = useAppSelector(
     (state) => !!state.checkedCards.IdCards.length,
   );
-  const condition = useAppSelector(
+  const condition: number = useAppSelector(
     (state) => state.checkedCards.IdCards.length,
+  );
+  const dataParse: CheckedCard[] = useAppSelector(
+    (state) => state.checkedCards.cards,
   );
 
   const { isDarkTheme } = useContext(ThemeContext);
+
+  const exportDataCSV = () => {
+    const csvData: string = Papa.unparse(dataParse);
+    const csvBlob: Blob = new Blob([csvData], { type: 'text/csv' });
+    const csvUrl: string = URL.createObjectURL(csvBlob);
+
+    const link: HTMLAnchorElement = document.createElement('a');
+    link.href = csvUrl;
+    link.download = `${dataParse.length}_episodes.csv`;
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    URL.revokeObjectURL(csvUrl);
+  };
 
   return (
     <div
@@ -29,6 +51,7 @@ const DropDownComponent: React.FC = () => {
         Cancel
       </button>
       <button
+        onClick={exportDataCSV}
         type="button"
         className={`drop-down-container__button drop-down-container__button ${isDarkTheme ? 'dark' : 'light'}`}
       >
